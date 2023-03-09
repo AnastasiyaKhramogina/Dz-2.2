e832902710d4e987c8ca5aa955e6e7848a8cb8e0
Anastasya Khramogina <akhramogina@mail.ru>
2023-03-09 20:18:45 +0300

    dz 2.2

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int x =10;
+       int y =1500;
+       int i = y / x;
+       int s = x + y + i;
+        if (y < 1000) {
+            System.out.println(x+y);
+        } else {
+            System.out.println(i);
+            System.out.println(s);
+        }
+    }
+}
