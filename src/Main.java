5c3c38fdc8def44006bc29eade7dbdae061ce145
Anastasya Khramogina <akhramogina@mail.ru>
2023-03-14 13:41:34 +0300

    finalized

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-       int startingScore =100;
-       int replenishment =1100;
-       int bonus = replenishment / startingScore;
-       int finalScore = startingScore + replenishment + bonus;
+        int startingScore = 100;
+        int replenishment = 1100;
+        int bonus = replenishment / startingScore;
+        int finalScore = startingScore + replenishment + bonus;
         if (replenishment < 1000) {
-            System.out.println(startingScore+replenishment);
+            System.out.println(startingScore + replenishment);
         } else {
             System.out.println(bonus);
             System.out.println(finalScore);
